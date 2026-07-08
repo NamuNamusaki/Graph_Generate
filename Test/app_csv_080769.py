779a39f2823fe0b311293df3aeda4a6544235cf1
NamuNamusaki <nam379740@gmail.com>
2026-07-08 13:38:06 +0700

    Update app_csv_080769.py

--- Test/app_csv_080769.py
+++ Test/app_csv_080769.py
@@ -7,6 +7,32 @@ import re
 
 st.set_page_config(page_title="Result Dashboard", layout="wide")
 
+#Password Part
+def check_password():
+    """Returns `True` if the user had a correct password."""
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input("Please enter the correct password:", 
+                      type="password", on_change=password_entered, key="password")
+        return False
+    elif not st.session_state["password_correct"]:
+        st.text_input("Please enter the correct password:",
+                      type="password", on_change=password_entered, key="password")
+        st.error(f"Password incorrect",color="red")
+        return False
+    else:
+        return True
+
+if not check_password():
+    st.stop()
 
 #Result Part
 st.title("Peptide Sequence Bioactivity Dashboard")
