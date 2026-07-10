316bef66a8ee2c46d0af87882e0d7bd6c953ff4f
NamuNamusaki <nam379740@gmail.com>
2026-07-10 16:41:55 +0700

    Update Upload.py

--- Demo_webapp/pages/Upload.py
+++ Demo_webapp/pages/Upload.py
@@ -6,6 +6,14 @@ st.markdown('Please upload your sequence')
 if not st.session_state.get('logged_in',False):
     st.warning('Please log in to view this page.')
     st.stop()
+# Sample Information
+project_name = st.text_input("Your Project Name")
+sample_name = st.text_input("Your Sample Name")
+organism = st.text_input("Your Organism")
+research_name = st.text_input("Researcher Name")
+institute = st.text_input("Institute/lab Name")
+description = st.text_area("Description / Objective / Remark")
+st.divider()
 
 # Upload File check
 if 'uploaded_files' not in st.session_state:
@@ -13,6 +21,17 @@ if 'uploaded_files' not in st.session_state:
 
 uploaded_files = st.file_uploader("Upload Your CSV files", type=['csv'], accept_multiple_files=True)
 st.divider()
+
+clevage_enz = st.selectbox('select one enzyme for insilico digestion', options=['Trypsin','Pepsin'])
+miss_cle = st.selectbox('Maximum missed cleavage sites allowed per peptide (Default: 4)',option=['0','1','2','3'])
+
+bioac_search = st.multiselect(
+                        "Select Bioactivities:",
+                    options=all_bioactivities,
+                    default=[],
+                    key=f"select_bioac_{group_name}"
+)
+ml_pred = st.multiselect('Applied to peptides with unknown bioactivity (Group 4)')
 
 if st.button('SUBMIT', type ='primary'):
     if uploaded_files:
